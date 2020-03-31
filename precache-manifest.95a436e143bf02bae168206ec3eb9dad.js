fe598b34bb04af5596df3988efa289da3d56734a
yuankui <yuankui64@gmail.com>
2020-04-01 00:11:43 +0800

    up

---
+++ precache-manifest.95a436e143bf02bae168206ec3eb9dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21cacb383ea23388eda06a1257172003",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12f65461ba229eb106ce",
+    "url": "/static/css/main.f17e5742.chunk.css"
+  },
+  {
+    "revision": "d8a05c50b34648bfd1cf",
+    "url": "/static/js/2.24c38809.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/static/js/2.24c38809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f65461ba229eb106ce",
+    "url": "/static/js/main.37ee587c.chunk.js"
+  },
+  {
+    "revision": "e6add165f6aae42b7160",
+    "url": "/static/js/runtime-main.ac8d9474.js"
+  }
+]);
